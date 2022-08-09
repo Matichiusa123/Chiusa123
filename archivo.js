cd74c0fa02a9cb92ffc8833d7669fedd06908b06
Matias Chiusano <mati.chiusano@hotmail.com>
2022-08-09 16:24:19 -0300

    entregadeltp

---
+++ archivo.js
@@ -0,0 +1,28 @@
+//CONSIGNA 1
+ /*
+for(let i=0; i < 10; i=i++){
+    let numero = parseInt(prompt("Ingrese un número y te voy a mostrar el siguiente"));
+    console.log("Ingresaste el numero:", numero);
+    console.log("El siguiente numero es:", numero + 1);
+}
+*/
+
+
+//CONSIGNA 2
+
+let nombre = "";
+let edad = "";
+let email = "";
+
+while (nombre, edad , email != "ESC"){ 
+    nombre = prompt ("Ingrese su nombre o ESC para finalizar");
+    edad = prompt ("Ingrese su edad o ESC para salir");
+    email = prompt ("Ingrese su email o ESC para salir");
+
+    console.log ("Bienvenido/a", nombre);
+    console.log ("Su edad es:", edad);
+    console.log("Su email es: ", email);
+}
+
+
+//CONSIGNA 3
