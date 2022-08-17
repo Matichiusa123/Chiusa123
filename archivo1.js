22fee24f2e9a1d8913bcb2945ea97ffa7f4baef1
Matias Chiusano <mati.chiusano@hotmail.com>
2022-08-17 12:21:15 -0300

    entrega segundo tp

---
+++ archivo1.js
@@ -0,0 +1,98 @@
+//Calcular el valor final de un producto en función de impuestos IVA
+
+
+/*function calcular_iva (precio){
+    let iva = precio * 0.21;
+    return iva
+}
+
+
+
+console.log ("Bienvenido a Chiusano Store");
+
+let producto = prompt("Ingrese el nombre del producto");
+let precio_producto = parseInt(prompt("Ingrese el precio del producto"));
+
+console.log("<-------------->");
+console.log("Nombre:", producto);
+console.log ("Precio:", precio_producto);
+
+let precio_mas_iva = precio_producto + calcular_iva(precio_producto);
+console.log("El precio con IVA es:", precio_mas_iva);
+
+let precio_menos_iva = precio_producto - calcular_iva(precio_producto);
+console.log("El precio con el IVA descontado es de:", precio_menos_iva);
+*/
+
+/*
+
+
+//Calcular pagos en cuotas sobre un monto determinado
+
+/*function divi (monto_producto, cant_cuota){
+    let resultado = monto_producto / cant_cuota
+    return resultado
+}
+
+
+console.log("Bienvenido/a");
+
+let monto_producto = parseInt(prompt("Ingrese el monto"));
+let cant_cuota = parseInt(prompt("Ingrese cantidad de cuotas: 3, 6 o 12"));
+console.log("Monto a pagar:", monto_producto);
+console.log("Cuotas a pagar:", cant_cuota);
+
+let monto_a_pagar = monto_producto / cant_cuota;
+console.log ("Monto final a pagar por mes:", monto_a_pagar);
+*/
+
+//Calcular total de productos seleccionados por el usuario
+
+/*
+function sumar (precio_producto, precio_producto_1, precio_producto_2){
+    let resultado = precio_producto + precio_producto_1 + precio_producto_2
+    return resultado
+}
+
+
+
+    console.log ("Bienvenido a Chiusano Store");
+
+let producto = prompt("Ingrese el nombre del producto");
+let producto_1 = prompt ("Ingrese otro producto");
+let producto_2 = prompt ("Ingrese el nombre de un producto más");
+
+let precio_producto = parseInt(prompt("Ingrese el precio del primer producto"));
+let precio_producto_1 = parseInt(prompt("Ingrese el precio del segundo producto"));
+let precio_producto_2 = parseInt(prompt("Ingrese el precio del tercer producto"));
+
+console.log("<-------------->");
+console.log("Nombre primer producto:", producto);
+console.log("Nombre segundo producto:", producto_1);
+console.log("Nombre del tercer producto:", producto_2);
+console.log ("Precio primer producto:", precio_producto);
+console.log("Precio segundo producto:", precio_producto_1);
+console.log("Precio tercer producto:", precio_producto_2);
+
+let precio_final = precio_producto + precio_producto_1 + precio_producto_2;
+console.log ("El precio total a abonar es de:", precio_final);
+*/
+
+//calcular edad promedio de los anotados
+
+
+console.log("Bienvenido/a");
+
+let edad_usuario_1 = parseInt(prompt("Indique su edad"));
+let edad_usuario_2 = parseInt(prompt("Indique la edad de su padre"));
+let edad_usuario_3 = parseInt(prompt("Indique la edad de su madre"));
+let edad_usuario_4 = parseInt(prompt("Indique la edad de su hermano"));
+
+console.log("Su edad es de:", edad_usuario_1);
+console.log("La edad de su padre es de:", edad_usuario_2);
+console.log("La edad de su madre es de:", edad_usuario_3);
+console.log("La edad de su hermano es de:", edad_usuario_4);
+
+let edad_promedio = (edad_usuario_1 + edad_usuario_2 + edad_usuario_3 + edad_usuario_4) / 4;
+console.log("La edad promedio de su familia es de:", edad_promedio);
+*/
